3028b299f39330f4160f1edff44ad1cca327d265
Sasan <l.s.arham@gmail.com>
2023-10-22 17:23:30 +0330

    [Add] Add a simple sender and receiver

---
+++ Receiver/Receiver/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receiver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "user",
+                Password = "password"
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: "direct_logs", type: ExchangeType.Direct);
+            // declare a server-named queue
+            var queueName = channel.QueueDeclare().QueueName;
+
+            var targetSeverities = new List<string> { "erro", "info", "debug" };
+
+            foreach (var severity in targetSeverities)
+            {
+                channel.QueueBind(queue: queueName,
+                                  exchange: "direct_logs",
+                                  routingKey: severity); // not only binds the queue with exchange but olso indicated the route that this queueis interested in 
+            }
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                var routingKey = ea.RoutingKey;
+                Console.WriteLine($" [x] Received '{routingKey}':'{message}'");
+            };
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ sender/sender/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace sender
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "user",
+                Password = "password"
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: "direct_logs", type: ExchangeType.Direct);
+
+            var severities = new List<string> { "erro", "warning", "info", "debug" };
+            Random rnd = new Random();
+
+            var severity = severities.ElementAt(rnd.Next(0, 3));
+            var message = "Hello";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "direct_logs",
+                                 routingKey: severity, // it means that thisexchange will push messages only to the queues that want message from this exchange with this routing
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine($" [x] Sent '{severity}':'{message}'");
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
